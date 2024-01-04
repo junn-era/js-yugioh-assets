f21a213c5ef18633b4fbf97f0ba8cc45e9ef4bb1
Junn-era <junnera@gmail.com>
2024-01-04 19:28:22 -0300

    Uploading files updated

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,24 +6,25 @@ const state = {
   },
   cardSprites: {
     avatar: document.getElementById("card-image"),
-    avatar: document.getElementById("card-name"),
-    avatar: document.getElementById("card-type"),
+    name: document.getElementById("card-name"),
+    type: document.getElementById("card-type"),
   },
   fieldCards: {
     player: document.getElementById("player-field-card"),
-    player: document.getElementById("computer-field-card"),
+    computer: document.getElementById("computer-field-card"),
   },
   action: {
     button:document.getElementById("next-duel"),
   },
 };
 
-const playersSides = {
-  player1: "player-field-card",
-  computer: "computer-field-card",
-}
+const playerSides = {
+  player: "player-cards",
+  computer: "computer-cards",
+};
 
-const pathImages = ".src/assets/icons/";
+const pathImages = "./src/assets/icons/";
+
 const cardData = [
   {
     Id: 0,
@@ -54,17 +55,49 @@ const cardData = [
 // const players = {
 //   player1: "player-cards",
 // }
+async function getRandomCardId() {
+  const randomIndex = Math.floor(Math.random() * cardData.length)
+  return cardData[randomIndex].Id;
+}
+
+async function createCardImage(IdCard, fieldSide) { 
+  const cardImage = document.createElement("img");
+  cardImage.setAttribute("height", "100px");
+  cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+  cardImage.setAttribute("data-id", IdCard);
+  cardImage.classList.add("card");
+
+  if(fieldSide === playerSides.player) {
+    cardImage.addEventListener("click", () => {
+      setCardsField(cardImage.getAttribute("data-id"));
+    });
+  }
+  
+    cardImage.addEventListener("mouseover", () => {
+      drawSelectCard(IdCard);
+    });
+
+  return cardImage;
+}
+
+
+async function drawSelectCard(index) {
+  state.cardSprites.avatar.src = cardData[index].img;
+  state.cardSprites.name.textContent = cardData[index].name;
+  state.cardSprites.type.textContent = "Attribute: " + cardData[index].type;
+}
+
 async function drawCards(cardNumbers, fieldSide) {
-  for(let i = 0; i < cardNumbers; i++) {
+  for (let i = 0; i < cardNumbers; i++) {
     const randomIdCard = await getRandomCardId();
     const cardImage = await createCardImage(randomIdCard, fieldSide);
     document.getElementById(fieldSide).appendChild(cardImage);
   }
 }
 
-
 function init() {
-  drawCards(5, "playerSides.player1");
-  drawCards(5, "playerSides.computer");
+  drawCards(5, playerSides.player);
+  drawCards(5, playerSides.computer);
 }
+
 init();
